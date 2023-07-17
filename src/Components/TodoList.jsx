a4ecc6b6af5271476014107f60cae02da528d325
TahsinMostafiz <mostafizur.webdesigner131@gmail.com>
2023-07-18 03:51:52 +0600

    fixed some bugs

--- src/Components/TodoList.jsx
+++ src/Components/TodoList.jsx
@@ -6,14 +6,14 @@ const TodoList = ({ task, i, deleteTask }) => {
     <>
       <div className=" border-b border-opacity-20 border-gray-700 bg-gray-800">
         <ul className="grid grid-cols-12">
-          <li className="px-3 py-2  col-span-1">
+          <li className="p-1 col-span-1 h-12">
             <span>{i + 1}</span>
           </li>
           <li className="px-3 py-2 col-span-8 ">
             <p>{task}</p>
           </li>
-          <li className="px-3 py-2 space-x-2 col-span-3 justify-self-end flex">
-            <div className="px-2 py-2 font-semibold rounded bg-green-500 text-white  leading-none">
+          <li className="p-2 space-x-2 col-span-3 flex justify-self-end h-12">
+            <div className="px-2 py-2 font-semibold rounded bg-green-500 text-white leading-none">
               <input
                 type="checkbox"
                 className="w-4 h-4 rounded-sm accent-white leading-none"
@@ -22,7 +22,7 @@ const TodoList = ({ task, i, deleteTask }) => {
             </div>
             <button
               onClick={() => deleteTask(i)}
-              className="px-2 py-2 font-semibold rounded bg-rose-500 text-white block"
+              className="px-2 py-2 font-semibold rounded bg-rose-500 text-white "
             >
               <FaTrash />
             </button>
